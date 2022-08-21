c0e8e8b1afdaead546d9210b6890dd8f72e19258
MugOfHotChoco <111291457+MugOfHotChoco@users.noreply.github.com>
2022-08-21 11:58:08 +0100

    Add files via upload

--- Example.py
+++ Example.py
@@ -1,8 +1,8 @@
 from AsciiEncrypter import *
-
+print("This is a demostation of some of the things you can use the Ascii Encrypter for.")
 print("----------------------------\n============================\n----------------------------");
 
-output = Encrypt("Hello World."); #Basic Encryption.#
+output = Encrypt("Hello World."); #Basic Encryption. (Remeber, it can only Encrypt Ascii characters.#
 print(output);
 output = Decrypt(output); #Basic Decryption.#
 print(output);
@@ -16,7 +16,7 @@ output = Decrypt(output,True); #Debugged Decryption.#
 
 print("----------------------------\n============================\n----------------------------");
 
-EncodingDict = {"h": "!", "e": "£","l": "$","o": "%"};
+EncodingDict = {"h": "!", "e": "£","l": "$","o": "%"}; #Each character corrosponds to it's encrypted form. E.g h -> !. Remeber that h lowercased and H uppercased are diffrent characters.#
 #{Orgchar1: EncryptedChar1, Orgchar2: EncryptedChar2, e.t.c};#
 output = Encrypt("hello",False,EncodingDict); #Specific Dictionary Encryption#
 print(output);
@@ -27,15 +27,15 @@ print("----------------------------\n============================\n-------------
 
 EncodingDict = {"h": "!", "e": "£","l": "$","o": "%"};
 EncodingDictChange(EncodingDict); #Changes Default Encoding Dictionary.#
-output = Encrypt("hello",False); #Encryption.#
+output = Encrypt("hello"); #Encryption.#
 print(output);
-output = Decrypt(output,False); #Decryption.#
+output = Decrypt(output); #Decryption.#
 print(output);
 
 print("----------------------------\n============================\n----------------------------");
 
-EncodingDictChange(DEFAULTDICT); #DefaultDict is a constant containting the Defualt Dictionary. This line of code is changing the Encoding and Decodiong Dictionary to the default.#
-output = Encrypt("hello",False); #Encryption.#
+EncodingDictChange(DEFAULTDICT); #DefaultDict is a constant containting the Defualt Ascii Encryption Dictionary. This line of code is changing the Encoding and Decoding Dictionary to the default.#
+output = Encrypt("hello"); #Encryption.#
 print(output);
-output = Decrypt(output,False); #Decryption.#
+output = Decrypt(output); #Decryption.#
 print(output);
